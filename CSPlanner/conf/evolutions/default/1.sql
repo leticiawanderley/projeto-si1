06e392e2332fc4c7c2959a07ab2a928e93d2e439
Júlio L <julio.leitao.jr@gmail.com>
2014-02-27 17:00:47 -0300

    Removendo projeto...apareceu do nada =/

--- CSPlanner/conf/evolutions/default/1.sql
+++ CSPlanner/conf/evolutions/default/1.sql
@@ -3,18 +3,11 @@
 
 # --- !Ups
 
-create table aluno)
-;
-
 create table disciplina (
-  id                        bigint not null,
   nome_da_disciplina        varchar(255),
   numero_de_creditos        integer,
-  dificuldade               integer,
-  constraint pk_disciplina primary key (id))
+  dificuldade_da_disciplina integer)
 ;
-
-create sequence disciplina_seq;
 
 
 
@@ -23,11 +16,7 @@ create sequence disciplina_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists aluno;
-
 drop table if exists disciplina;
 
 SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists disciplina_seq;
 
