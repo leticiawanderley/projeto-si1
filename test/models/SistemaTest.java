c04b96a5a3f35fc3a13f156c4fdefa5cbb9002c2
Julio L. Jr <julio.leitao.jr@gmail.com>
2014-03-16 12:01:45 -0300

    Adicionando testes do sistema

--- test/models/SistemaTest.java
+++ test/models/SistemaTest.java
@@ -2,13 +2,26 @@ package models;
 
 import static org.junit.Assert.*;
 
+import models.Grade.DisciplinasDSC;
+
 import org.junit.Test;
 
 public class SistemaTest {
 
-	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	@Test 
+	public void UsuarioTeste() {
+		Planejador planejador = new Planejador();
+		Aluno aluno = new Aluno("Maysa M.", "maysa.macedo95@gmail.com", "fafamaysa25");
+		
+		planejador.removeDisciplina(aluno, DisciplinasDSC.ALGEBRALINEAR.getDisciplina());
+		
+		/**
+		 * Remocao recursiva das disciplinas que eram dependentes de Algebra Linear
+		 */
+		assertFalse(planejador.getTodasDisciplinasDoAluno(aluno).contains(DisciplinasDSC.METODOS.getDisciplina()));
+		assertFalse(planejador.getTodasDisciplinasDoAluno(aluno).contains(DisciplinasDSC.IA.getDisciplina()));
+		assertFalse(planejador.getTodasDisciplinasDoAluno(aluno).contains(DisciplinasDSC.MSN.getDisciplina()));
+		
 	}
 
 }
