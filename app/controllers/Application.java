b1db482144ac20046f71b3a8acc8b7c863d60995
leticiawanderley <leticia.wanderley@ccc.ufcg.edu.br>
2014-03-21 01:08:38 -0300

    Adaptacoes ao multiusuario

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,5 @@
 package controllers;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
-
 import models.Aluno;
 import models.Disciplina;
 import models.User;
@@ -13,136 +9,159 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 
-import com.avaje.ebean.Ebean;
-
 /**
  * Classe que controla as requisições do sistema web
- *
+ * 
  */
 public class Application extends Controller {
-	
+
 	private static final String CADEIRA_NAO_EXISTENTE = "O nome da disciplina está incorreta ou não existe";
-			
+
 	private static GridSystem grid = new GridSystem();
-	private static Form<Disciplina> disciplinaForm = Form.form(Disciplina.class);
-	private static Finder<String, Aluno> usuariosLogados = new Finder<String, Aluno>(String.class, Aluno.class);
- 	private static Aluno USUARIO_NAO_LOGADO;
-	
+	private static Form<Disciplina> disciplinaForm = Form
+			.form(Disciplina.class);
+	private static Finder<String, Aluno> usuariosLogados = new Finder<String, Aluno>(
+			String.class, Aluno.class);
+	private static Aluno USUARIO_NAO_LOGADO;
+
 	/**
 	 * Pagina inicial da alocacao das disciplinas
+	 * 
 	 * @return um resultado/pagina que serah exibida no navegador
 	 */
 	@Security.Authenticated(Secured.class)
-    public static Result index() {
+	public static Result index() {
 		if (session("email") == null) {
 			return redirect(routes.Application.login());
 		}
-    	if (usuariosLogados.byId(session("email")) ==  USUARIO_NAO_LOGADO) {
-    		return redirect(routes.Application.login());
-    	}
-    	
-        return ok(views.html.index.render(usuariosLogados.byId(session("email")), disciplinaForm, grid.getPlanejador()));
-    }
-    
-    /**
-     * Pagina para visualizar a grade do DSC
-     * @return um resultado/pagina que serah exibida no navegador
-     */
-    public static Result verGrade() {
-    	return ok(views.html.visualizaGrade.render());
-    }
-    
-    /**
-     * Pagina para visualizar as disciplinas disponiveis para matricula
-     * @return um resultado/pagina que serah exibida no navegador
-     */
-    public static Result selecionarDisciplinas() {
-    	return ok(views.html.disciplinasDoCurso.render(usuariosLogados.byId(session("email")), disciplinaForm,grid.getPlanejador()));
-    }
-
-    /**
-     * Matricula o usuario na disciplina
-     * @return um resultado/pagina que serah exibida no navegador
-     */
-    public static Result matriculaNaDisciplina(int periodo, String disciplina) {
-    	Form<Disciplina> filledForm = disciplinaForm.bindFromRequest();
-    	if (filledForm.hasErrors()) {
-			return badRequest(views.html.index.render(usuariosLogados.byId(session("email")), disciplinaForm, grid.getPlanejador()));
-		} else {
-			grid.getPlanejador().alteraPeriodoDaDisciplina(usuariosLogados.byId(session("email")), grid.getPlanejador().getDisciplina(disciplina), periodo);
+		if (usuariosLogados.byId(session("email")) == USUARIO_NAO_LOGADO) {
+			return redirect(routes.Application.login());
 		}
-    	return redirect("/");
-    }
+
+		return ok(views.html.index.render(
+				usuariosLogados.byId(session("email")), disciplinaForm,
+				grid.getPlanejador()));
+	}
 
 	/**
-     * Remove uma disciplina do usuario
-     * @return um resultado/pagina que serah exibida no navegador
-     */
+	 * Pagina para visualizar a grade do DSC
+	 * 
+	 * @return um resultado/pagina que serah exibida no navegador
+	 */
+	public static Result verGrade() {
+		return ok(views.html.visualizaGrade.render());
+	}
+
+	/**
+	 * Pagina para visualizar as disciplinas disponiveis para matricula
+	 * 
+	 * @return um resultado/pagina que serah exibida no navegador
+	 */
+	public static Result selecionarDisciplinas() {
+		return ok(views.html.disciplinasDoCurso.render(
+				usuariosLogados.byId(session("email")), disciplinaForm,
+				grid.getPlanejador()));
+	}
+
+	/**
+	 * Matricula o usuario na disciplina
+	 * 
+	 * @return um resultado/pagina que serah exibida no navegador
+	 */
+	public static Result matriculaNaDisciplina(int periodo, String disciplina) {
+		Form<Disciplina> filledForm = disciplinaForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(
+					usuariosLogados.byId(session("email")), disciplinaForm,
+					grid.getPlanejador()));
+		} else {
+			grid.getPlanejador().alteraPeriodoDaDisciplina(
+					usuariosLogados.byId(session("email")),
+					grid.getPlanejador().getDisciplina(disciplina), periodo);
+		}
+		return redirect("/");
+	}
+
+	/**
+	 * Remove uma disciplina do usuario
+	 * 
+	 * @return um resultado/pagina que serah exibida no navegador
+	 */
 	public static Result removeDisciplina(String nomeDaDisciplina) {
-		Disciplina disciplina = grid.getPlanejador().getDisciplina(nomeDaDisciplina);
+		Disciplina disciplina = grid.getPlanejador().getDisciplina(
+				nomeDaDisciplina);
 		if (!grid.getPlanejador().existeCadeira(nomeDaDisciplina)) {
-    		flash("sucess", CADEIRA_NAO_EXISTENTE);
-    		return badRequest(views.html.index.render(usuariosLogados.byId(session("email")), disciplinaForm, grid.getPlanejador()));
-    	}
-		grid.getPlanejador().removeDisciplinaESeusPreRequisitos(usuariosLogados.byId(session("email")), disciplina);
-		((Aluno)usuariosLogados.byId(session("email"))).update();
+			flash("sucess", CADEIRA_NAO_EXISTENTE);
+			return badRequest(views.html.index.render(
+					usuariosLogados.byId(session("email")), disciplinaForm,
+					grid.getPlanejador()));
+		}
+		grid.getPlanejador().removeDisciplinaESeusPreRequisitos(
+				usuariosLogados.byId(session("email")), disciplina);
+		((Aluno) usuariosLogados.byId(session("email"))).update();
 		return index();
-    }
-	
+	}
+
 	public static Result cadastrarNovoUsuario() {
 		return ok(views.html.cadastrarUsuario.render(Form.form(User.class)));
 	}
-	
+
 	public static Result criarUsuario() {
 		Form<User> loginForm = Form.form(User.class).bindFromRequest();
-		if (!loginForm.get().getPassword().equals(loginForm.data().get("confirmPassword"))) {
+		if (!loginForm.get().getPassword()
+				.equals(loginForm.data().get("confirmPassword"))) {
 			flash("success", "Senha e confirmação de senha estão incompativeis");
 			return cadastrarNovoUsuario();
 		}
-    	Aluno novoAluno = new Aluno(loginForm.get().getName(), loginForm.get().getEmail(), loginForm.get().getPassword());
-    	if (usuariosLogados.byId(novoAluno.getEmail()) != null) {
-    		flash("success", "Usuário já existente no sistema, utilize outro e-mail");
+		Aluno novoAluno = new Aluno(loginForm.get().getName(), loginForm.get()
+				.getEmail(), loginForm.get().getPassword());
+		if (usuariosLogados.byId(novoAluno.getEmail()) != null) {
+			flash("success",
+					"Usuário já existente no sistema, utilize outro e-mail");
 			return cadastrarNovoUsuario();
-    	}
-    	grid.alocandoNovoUsuario(novoAluno);
-    	novoAluno.save();
-    	session().clear();
-    	session("email", novoAluno.getEmail());
-    	return index();
+		}
+		grid.alocandoNovoUsuario(novoAluno);
+		novoAluno.save();
+		session().clear();
+		session("email", novoAluno.getEmail());
+		return index();
 	}
+
 	public static Result setarPeriodoAtual(int periodoAtual) {
-		((Aluno) usuariosLogados.byId(session("email"))).setPeriodoAtual(periodoAtual);
+		usuariosLogados.byId(session("email")).setPeriodoAtual(periodoAtual);
+		usuariosLogados.byId(session("email")).update();
 		return redirect("/");
 	}
-	
+
 	public static Result login() {
-	    return ok(views.html.login.render(Form.form(User.class)));
+		return ok(views.html.login.render(Form.form(User.class)));
 	}
-	
+
 	public static Result logout() {
-	    session().clear();
-	    flash("success", "You've been logged out");
-	    return redirect(routes.Application.login());
+		session().clear();
+		flash("success", "You've been logged out");
+		return redirect(routes.Application.login());
 	}
-	
+
 	public static Result authenticate() {
-	    Form<User> loginForm = Form.form(User.class).bindFromRequest();
-	    if (loginForm.hasErrors()) {
-	        return badRequest(views.html.login.render(loginForm));
-	    } else {
-	    	if (usuariosLogados.byId(loginForm.get().getEmail()) == null) {
-	    		flash("success", "Usuário não existente");
-	    	} else {
-	    		if (!usuariosLogados.byId(loginForm.get().getEmail()).getPassword().equals(loginForm.get().getPassword())) {
-	    			flash("success", "Senha incorreta, tente novamente");
-	    		} else {
-	    			session().clear();
-    		        session("email", loginForm.get().getEmail());
-    		        return redirect(routes.Application.index());
-	    		}
-	    	}
-	    }
-	    return badRequest(views.html.login.render(Form.form(User.class)));
+		Form<User> loginForm = Form.form(User.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(views.html.login.render(loginForm));
+		} else {
+			if (usuariosLogados.byId(loginForm.get().getEmail()) == null) {
+				flash("success", "Usuário não existente");
+			} else {
+				if (!usuariosLogados.byId(loginForm.get().getEmail())
+						.getPassword().equals(loginForm.get().getPassword())) {
+					flash("success", "Senha incorreta, tente novamente");
+				} else {
+					session().clear();
+					session("email", loginForm.get().getEmail());
+					return redirect(routes.Application.index());
+				}
+			}
+		}
+		return badRequest(views.html.login.render(Form.form(User.class)));
 	}
-    
+
 }
--- app/models/Aluno.java
+++ app/models/Aluno.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -11,30 +12,34 @@ import models.validador.ValidacaoCreditos;
 
 /**
  * Classe que representa um aluno
- *
+ * 
  */
 @Entity
 public class Aluno extends User {
 
-	// INFORMATION EXPERT - contém as informacoes do aluno:nomeDoAluno,listaDePeriodos .
-	
+	// INFORMATION EXPERT - contém as informacoes do
+	// aluno:nomeDoAluno,listaDePeriodos .
+
 	private static final long serialVersionUID = 7507028957989504099L;
 	private static final int PRIMEIRO_PERIODO = 0;
 	private static final int ULTIMO_PERIODO = 9;
 	@OneToMany(cascade = CascadeType.ALL)
 	private List<Periodo> listaDePeriodo;
+	@Column
 	private int periodoAtual;
-	
+
 	/**
-	 * Construtor da classe	
-	 * @param nome Nome do aluno
+	 * Construtor da classe
+	 * 
+	 * @param nome
+	 *            Nome do aluno
 	 */
 	public Aluno(String name, String email, String password) {
 		super(name, email, password);
 		this.listaDePeriodo = new ArrayList<Periodo>();
 		this.periodoAtual = PRIMEIRO_PERIODO;
 	}
-	
+
 	/**
 	 * 
 	 * @return a lista dos periodos do aluno
@@ -53,7 +58,8 @@ public class Aluno extends User {
 
 	/**
 	 * 
-	 * @param periodoAtual o periodo em que o aluno estah atualmente
+	 * @param periodoAtual
+	 *            o periodo em que o aluno estah atualmente
 	 */
 	public void setPeriodoAtual(int periodoAtual) {
 		this.periodoAtual = periodoAtual;
@@ -61,9 +67,11 @@ public class Aluno extends User {
 			listaDePeriodo.get(i).setValidadorDoPeriodo(ValidacaoCreditos.MAX);
 		}
 		for (int i = periodoAtual; i < listaDePeriodo.size(); i++) {
-			listaDePeriodo.get(i).setValidadorDoPeriodo(ValidacaoCreditos.MINMAX);
+			listaDePeriodo.get(i).setValidadorDoPeriodo(
+					ValidacaoCreditos.MINMAX);
 		}
-		listaDePeriodo.get(ULTIMO_PERIODO).setValidadorDoPeriodo(ValidacaoCreditos.MIN);
+		listaDePeriodo.get(ULTIMO_PERIODO).setValidadorDoPeriodo(
+				ValidacaoCreditos.MIN);
 	}
-	
+
 }
