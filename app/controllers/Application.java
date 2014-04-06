8b4f531aef5b0f90536dfdd186b0e4e7fe093e58
leticiawanderley <leticia.wanderley@ccc.ufcg.edu.br>
2014-04-06 20:53:54 -0300

    Correções em Application e routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,5 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import models.Aluno;
 import models.Disciplina;
 import models.Usuario;
